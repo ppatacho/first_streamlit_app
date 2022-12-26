9e5f3760b799c40d8f57f0e379315176a4838d0c
ppatacho <121431437+ppatacho@users.noreply.github.com>
2022-12-26 12:32:23 +0100

    Update streamliy.app.py
    Added content on breakfast

--- streamliy.app.py
+++ streamliy.app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Meny')
+streamlit.text('Omega 3')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.teXt('Hard-Boiled Free-Range Egg')
